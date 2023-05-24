d1b4d8e1bef74518fc15a0488de8e94bfffe4b00
Chuck Song <chuckthedeveloper@gmail.com>
2023-05-23 23:40:29 -0400

    Cteared login modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app';
 
 import Layout from '@/components/Layout';
+import LoginModal from '@/components/modals/LoginModal';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
