b158a987e8f659fe038fe18936e43d426080fd7c
Chuck Song <chuckthedeveloper@gmail.com>
2023-05-25 22:26:59 -0400

    Added sign in footer toggle

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -14,6 +14,15 @@ const RegisterModal = () => {
   const [username, setUsername] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
+  const onToggle = useCallback(() => {
+    if (isLoading) {
+      return;
+    }
+
+    registerModal.onClose();
+    LoginModal.onOpen();
+  }, [isLoading, registerModal, LoginModal])
+
   const onSubmit = useCallback(async () => {
     try {
       setIsLoading(true);
@@ -57,6 +66,21 @@ const RegisterModal = () => {
     </div>
   )
 
+  const footerContent = (
+    <div className="text-neutral-400 text-center mt-4">
+      <p> Already have an account?
+        <span
+          onClick={onToggle}
+          className="
+          text-white
+          cursor-pointer
+          hover:underline
+          "
+        > Sign in </span>
+      </p>
+    </div>
+  )
+
   return (
     <Modal
       disabled={isLoading}
@@ -66,6 +90,7 @@ const RegisterModal = () => {
       onClose={registerModal.onClose}
       onSubmit={onSubmit}
       body={bodyContent}
+      footer={footerContent}
     />
   )
 }
