a3c1627a3513c4dab1dfafbc5b9e77903e50e3e7
Chuck Song <chuckthedeveloper@gmail.com>
2023-06-01 23:38:04 -0400

    Implemented register function

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react';
 
 import Layout from '@/components/Layout';
 import RegisterModal from '@/components/modals/RegisterModal';
@@ -7,13 +9,14 @@ import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
+      <Toaster />
       <RegisterModal />
       <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SessionProvider>
   )
 }
 
